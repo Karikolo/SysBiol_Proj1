18e34d90d97186b336f08579f60e0ef087417a5d
Karikolo <kj.kolodzie2@students.uw.edu.pl>
2025-04-01 12:38:16 +0200

    Age updated (they should be dying after 5 generations, as well as children added to visualisation

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ import numpy as np
 # PARAMETRY POPULACJI
 # -------------------
 K = 250           # 200 limit pojemności siedliska
-N = 17           # liczba osobników w populacji
+N = 20           # liczba osobników w populacji
 n = 2            # wymiar przestrzeni fenotypowej
 lifespan = 5
 
 # --------------------
 # PARAMETRY MUTACJI
 # --------------------
-mu = 0.1         # prawdopodobieństwo mutacji dla osobnika (czy)c
-mu_c = 0.5       # prawdopodobieństwo mutacji konkretnej cechy, jeśli osobnik mutuje (która cecha)
-xi = 0.3        # odchylenie standardowe w rozkładzie normalnym mutacji (o ile)
+mu = 0.8         # prawdopodobieństwo mutacji dla osobnika
+mu_c = 0.7       # 0,7 prawdopodobieństwo mutacji konkretnej cechy, jeśli osobnik mutuje
+xi = 0.3        # odchylenie standardowe w rozkładzie normalnym mutacji
 
 # --------------------
 # PARAMETRY SELEKCJI
@@ -31,12 +31,13 @@ threshold_asex = 0.9 # 0,9próg do selekcji progowej osobników mogących się r
 alpha0 = np.array([0.0, 0.0])  
 # Wektor kierunkowej zmiany c
 c = np.array([0.01, 0.01])     # [0.01, 0.01]
-delta = 0.05    # odchylenie standardowe dla fluktuacji
-max_generations = 150  # liczba pokoleń do zasymulowania
+delta = 0.15    # odchylenie standardowe dla fluktuacji
+max_generations = 200  # 20 liczba pokoleń do zasymulowania
+seed = 10 # używane dla porównywania tych samych symulacji w analizach
 
 # ----------------------
 # PARAMETRY REPRODUKCJI
 # ----------------------
-avg_children = 1.5
+avg_children = 2
 # W wersji bezpłciowej zakładamy klonowanie z uwzględnieniem mutacji.
 # Jeśli chcemy modelować płciowo, trzeba dodać odpowiednie parametry.
